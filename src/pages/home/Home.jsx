c2ea7e1d42fc9da60f6ef7105d05a405dd82fd1b
hamymah <hanymah823@gmail.com>
2023-10-09 03:39:32 +0200

    add the testimonial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import HeroSection from '../../components/heroSection/HeroSection'
 import Filter from '../../components/filter/Filter'
 import ProductCard from '../../components/productCard/ProductCard'
 import Track from '../../components/track/Track'
+import Testimonial from '../../components/testimonial/Testimonial'
 
 
 function Home() {
@@ -16,6 +17,7 @@ function Home() {
       <Filter/>
       <ProductCard/>
       <Track/>
+      <Testimonial/>
     </Layout>
   )
 }

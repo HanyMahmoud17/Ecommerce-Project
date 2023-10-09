57b77d6da4ef9dafdb5157b3ec8793b16996d1ec
hamymah <hanymah823@gmail.com>
2023-10-09 03:34:21 +0200

    add the filter ui and product cards ui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Layout from '../../components/layout/Layout'
 // import MyContext from '../../context/data/myContext'
 import MyContext from '../../context/data/myContext'
 import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
 
 
 function Home() {
@@ -10,6 +12,8 @@ function Home() {
   return (
     <Layout>
       <HeroSection/>
+      <Filter/>
+      <ProductCard/>
     </Layout>
   )
 }

80c2ce43c323edfefe9d156ec322454d5969e596
hamymah <hanymah823@gmail.com>
2023-10-19 10:00:55 +0200

    make the schema for product and make the function to show the products in my  state

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MyContext from './myContext';
+import { toast } from 'react-toastify';
+import { Timestamp, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { fireDB } from '../../firebase/FirebaseConfig';
 
 function MyState(props) {
   const [mode,setMode]=useState('light')
+  // this is the loading state
+  const [loading,setLoading]=useState(false)
+
 
   const toggleMode =()=>{
     if(mode=='light'){
@@ -15,13 +21,80 @@ function MyState(props) {
     }
   }
 
-  // this is the loading state
-  const [loading,setLoading]=useState(false)
+
+
+  // product 
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+
+  })
+  // ********************** Add Product Section  **********************
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error('Please fill all fields')
+    }
+    const productRef = collection(fireDB, "products")
+    setLoading(true)
+    try {
+      await addDoc(productRef, products)
+      toast.success("Product Add successfully")
+      getProductData()
+      closeModal()
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+    setProducts("")
+  }
+
+  const [product, setProduct] = useState([]);
+
+  // ****** get product
+  const getProductData = async () => {
+    setLoading(true)
+    try {
+      const q = query(
+        collection(fireDB, "products"),
+        orderBy("time"),
+        // limit(5)
+      );
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productsArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productsArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProduct(productsArray)
+        setLoading(false);
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getProductData();
+  }, []);
 
 
   return (
     // i here pass the value that i need 
-    <MyContext.Provider value={{mode,toggleMode,loading,setLoading }}>
+    <MyContext.Provider value={{mode,toggleMode,loading,setLoading, products, setProducts,addProduct }}>
         {props.children}
     </MyContext.Provider>
   )

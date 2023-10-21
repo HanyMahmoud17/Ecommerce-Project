abfa479273be70ec6c16f5d94f1713e36d7b5f97
hamymah <hanymah823@gmail.com>
2023-10-21 20:36:25 +0200

    make the order page and its function

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -137,14 +137,37 @@ function MyState(props) {
     }
   }
 
+  // order
+  const [order, setOrder] = useState([]);
+
+  const getOrderData = async () => {
+    setLoading(true)
+    try {
+      const result = await getDocs(collection(fireDb, "orders"))
+      const ordersArray = [];
+      result.forEach((doc) => {
+        ordersArray.push(doc.data());
+        setLoading(false)
+      });
+      setOrder(ordersArray);
+      console.log(ordersArray)
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+
   useEffect(() => {
     getProductData();
+    getOrderData();
   }, []);
 
 
   return (
     // i here pass the value that i need 
-    <MyContext.Provider value={{mode,toggleMode,loading,setLoading, product,products, setProducts,addProduct,edithandle,updateProduct,deleteProduct }}>
+    <MyContext.Provider value={{mode,toggleMode,loading,setLoading, product,products, setProducts,addProduct,edithandle,updateProduct,deleteProduct,order }}>
         {props.children}
     </MyContext.Provider>
   )
